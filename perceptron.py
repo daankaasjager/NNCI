1a1f8e6c41f4ff20a6ea72f42651cc30fda1c369
daankaasjager <daankaasjager@hotmail.com>
2023-11-30 14:05:59 +0100

    finished part c

--- perceptron.py
+++ perceptron.py
@@ -14,12 +14,32 @@ def generate_dataset():
     return xi_vector_set
 
 
+def initialize_weights():
+    return np.zeros((P, N))
+
+
+def calculate_Ev():
+    pass
+
+
+def update_weight(weight_vector, input_vector, vector_label):
+    weight_vector += (1/N)*input_vector*vector_label
+
+
 def train(data):
     sweep_count = 0
+    w = initialize_weights()
     while sweep_count != MAX_SWEEPS:
+        no_updates = True
         for i in range(P):
-            print(data[i])
-            print("hi")
+            if np.dot(w[i], data[i][0]) * data[i][1] <= 0:
+                update_weight(w[i], data[i][0], data[i][1])
+                no_updates = False
+
+        if no_updates:
+            print("Finished training")
+            break
+        print("sweepcount: ")
         print(sweep_count := sweep_count + 1)
 
 
